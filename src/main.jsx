467d40e6e8b59e59e92e15627322c225f382f1ce
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2024-07-17 01:57:29 +0600

    правка после разбора ДЗ на лекции

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,43 @@
 import ReactDOM from "react-dom/client";
-import { restaurants } from "./mock.js";
+import { restaurants } from "./constants/mock.js";
 
-const rootElement = document.getElementById("root");
-const reactRoot = ReactDOM.createRoot(rootElement);
-reactRoot.render(
-    restaurants.map((restaurant) => (
-        <div>
-            <h2>{restaurant.name}</h2>
-            <h3>Menu</h3>
-            {restaurant.menu.map((dish) => (
-                <ul>
-                    <li>
-                        <h4>
-                            {dish.name} - ${dish.price}
-                        </h4>
-                        <h5>Ingredients</h5>
-                        {dish.ingredients.map((ingridient) => (
-                            <ul>
-                                <li>{ingridient}</li>
-                            </ul>
-                        ))}
-                    </li>
-                </ul>
-            ))}
-            <h3>Reviews</h3>
-            {restaurant.reviews.map((review) => (
-                <ul>
-                    <li>
-                        {review.user} ({review.rating}): {review.text}
-                    </li>
-                </ul>
-            ))}
-        </div>
-    ))
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <div>
+        <h1>Reastaurant page</h1>
+        {restaurants.map(({ name, menu, reviews }) => {
+            return (
+                <div>
+                    <h2>{name}</h2>
+                    <h3>Menu</h3>
+                    <ul>
+                        {menu.map((dish) => {
+                            return (
+                                <li>
+                                    <h4>
+                                        {dish.name} - ${dish.price}
+                                    </h4>
+                                    <h5>Ingredients</h5>
+                                    <ul>
+                                        {dish.ingredients.map((ingridient) => (
+                                            <li>{ingridient}</li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <h3>Reviews</h3>
+                    <ul>
+                        {reviews.map((review) => {
+                            return (
+                                <li>
+                                    {review.user} ({review.rating}): {review.text}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            );
+        })}
+    </div>
 );
