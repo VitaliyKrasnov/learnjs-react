a72ef870dc4d9497625470c80f6d84884cd89a0f
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2024-07-23 04:28:21 +0600

    ДЗ-2: изменено условие определения активного ресторана

--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -5,19 +5,19 @@ import { Header } from "../header/component.jsx";
 import { Restaurant } from "../restaurant/component.jsx";
 
 export const Layout = ({ restaurants }) => {
-    const [activeRestaurant, setActiveRestaurant] = useState({ name: restaurants[0].name });
-    const setActiveRestaurantName = (name) => setActiveRestaurant({ name: name });
-    let activeRestaurantObj = restaurants.find((restaurant) => restaurant.name === activeRestaurant.name);
+    const [activeRestaurant, setActiveRestaurant] = useState({ id: restaurants[0].id });
+    const setActiveRestaurantId = (id) => setActiveRestaurant({ id: id });
+    let activeRestaurantObj = restaurants.find((restaurant) => restaurant.id === activeRestaurant.id);
 
     return (
         <div>
             <Header />
-            {restaurants.map(({ name }) => {
+            {restaurants.map(({ name, id }) => {
                 return (
-                    <Tab name={name} clickHandler={() => setActiveRestaurantName(name)} />
+                    <Tab name={name} clickHandler={() => setActiveRestaurantId(id)} />
                 );
             })}
-            <Restaurant activeRestaurant={activeRestaurant} restaurant={activeRestaurantObj} />
+            <Restaurant restaurant={activeRestaurantObj} />
             <Footer />
         </div>
     );
