85bd78b813a29fa502b1d60012068a269c0f8480
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2024-07-31 02:01:15 +0600

    добавлен компонент-контейнер для счётчика

---
+++ src/components/counter/container.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Counter } from "./component.jsx";
+
+const useCounter = (min, max, step) => {
+    const [value, setValue] = useState(min);
+    const increment = () => setValue((prevState) => Math.min(prevState + step, max));
+    const decrement = () => setValue((prevState) => Math.max(prevState - step, min));
+    
+    return {
+        value,
+        increment,
+        decrement
+    };
+};
+
+export const CounterContainer = ({ min=0, max=9, step=1 }) => {
+    const {value, increment, decrement} = useCounter(min, max, step);
+    
+    return (
+        <Counter value={value} increment={increment} decrement={decrement} />
+    );
+};
