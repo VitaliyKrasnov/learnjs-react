f9f061d238bc1a32f7277ef4a1edc3fc2ce9aba1
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2024-08-02 02:24:19 +0600

    ДЗ-3: исправление замечаний

--- src/components/scrollProgressBar/component.jsx
+++ src/components/scrollProgressBar/component.jsx
@@ -1,34 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const calculateProgressBarWidth = () => {
+    const maxHeight = document.documentElement.scrollHeight;
+    const sizeHeight = window.innerHeight;
+    const scrolls = window.scrollY;
+    return (scrolls / (maxHeight - sizeHeight)) * 100;
+};
+
+const useScrollProgressBar = () => {
+    const [width, setWidth] = useState("0%");
+
+    useEffect(() => {
+        const sideEffectFunction = () => {
+            setWidth(`${calculateProgressBarWidth()}%`);    
+        };
+        document.addEventListener("scroll", sideEffectFunction);
+        return () => {
+            document.removeEventListener("scroll", sideEffectFunction);
+        };
+    }, []);
+
+    return width;
+};
 
 export const ScrollProgressBar = () => {
+    const progressBarWidth = useScrollProgressBar();
     return (
         <div
-            id="scrollProgressBar"
             style={{
                 backgroundColor: "#2bc10d",
                 position: "fixed",
                 zIndex: "99999",
-                width: "0%",
+                width: progressBarWidth,
                 height: "4px",
                 top: "0",
                 left: "0",
             }}
-        >
-            {useEffect(() => {
-                const calculateScrollProgress = () => {
-                    const maxHeight = document.body.scrollHeight;
-                    const sizeHeight = window.innerHeight;
-                    const scrolls = window.scrollY;
-                    document.getElementById("scrollProgressBar").style.width = `${
-                        (scrolls / (maxHeight - sizeHeight)) * 100
-                    }%`;
-                };
-                document.addEventListener("scroll", calculateScrollProgress);
-
-                return () => {
-                    document.removeEventListener("scroll", calculateScrollProgress);
-                };
-            }, [])}
-        </div>
+        ></div>
     );
 };
